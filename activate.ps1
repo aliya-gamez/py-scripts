669b1f7ed0317a2ace6aacdb49edc2836b688d0e
Aliya Gamez <aliya.e.gamez@gmail.com>
2026-04-08 13:19:13 -0400

    update scripts

--- activate.ps1
+++ activate.ps1
@@ -12,18 +12,12 @@ else {
 }
 
 # Virtual Environment path creation/check
-if (-not (Test-Path 'venv')) {
-    Write-Host 'Virtual environment path not found, creating now...' -ForegroundColor Yellow
-    python -m venv venv
+if (-not (Test-Path '.venv')) {
+    Write-Host 'Virtual environment path not found.' -ForegroundColor Red
+    exit 1
 }
 else {
     Write-Host 'Virtual environment path found' -ForegroundColor Green
-}
-
-# Verify: Virtual Environment path creation/check
-if (-not (Test-Path 'venv')) {
-    Write-Host 'Virtual environment path failed to create normally.' -ForegroundColor Red
-    exit 1
 }
 
 # Virtual environment activation/check
@@ -40,19 +34,6 @@ if (!$env:VIRTUAL_ENV) {
     Write-Host 'Virtual environment failed to activate.' -ForegroundColor Red
     exit 1
 }
-
-# Verify: Requirements file check
-if (!(Test-Path 'requirements.txt')) {
-    Write-Host 'requirements.txt not found' -ForegroundColor Red
-    exit 1
-}
-
-# Installs
-Write-Host 'Installing dependencies...' -ForegroundColor Green
-Write-Host ''
-
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
 
 Write-Host ''
 
